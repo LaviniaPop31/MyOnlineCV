eedf07db8dac62e90edf4286d23fe8f9cd8ef662
LaviniaPop31 <laviniapop31@gmail.com>
2021-02-26 23:41:24 +0200

    implemented js for contact page

---
+++ Contact_page/contact.js
@@ -0,0 +1,47 @@
+let inputName = document.getElementById('inputName');
+let inputEmail = document.getElementById('inputEmail');
+let inputSubject = document.getElementById('inputSubject');
+let inputMessage = document.getElementById('inputMessage');
+let button = document.getElementById('sendButton')
+let displayMessage = document.getElementById('showMessage');
+
+button.addEventListener('click', () => {
+    console.log(inputName.value)
+    if (inputName.value === '' || inputEmail.value === '' || inputSubject.value === '' || inputMessage.value === '') {
+        displayMessage.innerHTML = 'You have to fill all the fields'
+    } else {
+        sendEmail()
+        displayMessage.innerHTML = 'Your message has been sent!';
+        document.querySelector('form').reset();
+    }
+})
+
+
+function sendEmail() {
+    const baseUrl = 'https://formspree.io/f/xnqokjgz';
+
+    let contentInformation = {
+        name: inputName.value,
+        email: inputEmail.value,
+        subject: inputSubject.value,
+        message: inputMessage.value
+    }
+
+    fetch(baseUrl, {
+        method: 'POST',
+        body: JSON.stringify(contentInformation),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+
+    }).then(function (response) {
+        console.log(response)
+        if (response.status === 404) {
+            displayMessage.innerHTML = "Error! Your message hasn't been sent!"
+        } else if (response.status === 200) {
+        }
+        return response.json()
+    }).catch(error => {
+        displayMessage.innerHTML = "Error! Your message hasn't been sent!"
+    })
+}
