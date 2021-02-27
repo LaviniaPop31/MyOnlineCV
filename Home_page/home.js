986ab45220b77f871af4ede6c4e18a7d384e2918
LaviniaPop31 <laviniapop31@gmail.com>
2021-02-27 15:37:37 +0200

    another .

--- Home_page/home.js
+++ Home_page/home.js
@@ -10,63 +10,17 @@ function transitionEffect() {
 
         button.addEventListener('click', (e) => {
             e.preventDefault();
-    
-
+            // let target = e.target.href
+            // console.log(target)
             transitionElement.classList.add('is-active');
 
             setTimeout(() => {
-                window.location.href ='./About_page/about.html'
+                window.location.href ='../About_page/about.html'
+        
             }, 500)
         })
 
     }
 }
 
-// displayHomePage()
-// function displayHomePage() {
-//     let homeButton = document.getElementById('homeButton');
-//     homeButton.addEventListener('click', () => {
-//         window.location.href = '/Home_page/home.html'
-//     })
-// }
-
-// displayAboutPage()
-// function displayAboutPage() {
-//     let aboutButton = document.getElementById('aboutButton');
-//     aboutButton.addEventListener('click', () => {
-//         window.location.href = '/About_page/about.html'
-//     })
-// }
-
-// displayResumePage()
-// function displayResumePage() {
-//     let resumeButton = document.getElementById('resumeButton');
-//     resumeButton.addEventListener('click', () => {
-//         window.location.href = '/Resume_page/resume.html';
-//     })
-// }
-
-// displayContactPage()
-// function displayContactPage() {
-//     let contactButton = document.getElementById('contactButton');
-//     contactButton.addEventListener('click', () => {
-//         window.location.href = '/Contact_page/contact.html'
-//     })
-// }
-
-// displayInterestsPage()
-// function displayInterestsPage() {
-//     let interestsButton = document.getElementById('interestsButton');
-//     interestsButton.addEventListener('click', () => {
-//         window.location.href = '/Interests_page/interests.html'
-//     })
-// }
-
-// displayPortofolioPage()
-// function displayPortofolioPage() {
-//     let portofolioButton = document.getElementById('portofolioButton');
-//     portofolioButton.addEventListener('click', () => {
-//         window.location.href = '/Portofolio_page/portofolio.html'
-//     })
-// }
 
