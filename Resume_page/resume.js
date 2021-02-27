125eeaa5fb7941cfed57d3be9d8fcaf02c488c96
LaviniaPop31 <laviniapop31@gmail.com>
2021-02-27 13:36:04 +0200

    minor changes

--- Resume_page/resume.js
+++
@@ -1,7 +0,0 @@
-// debugger
-var collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
-console.log(collapseElementList)
-collapseElementList.map(function (collapseEl) {
-  return new bootstrap.Collapse(collapseEl)
-})
-
